33f3afbad608fff78ebddda025e16deed5a986f4
Chris Rayner <chrisraynerr@gmail.com>
2024-08-13 21:28:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-const images = [
-    "KINGDOM/AJP_Burberry-SS_03.jpg",
-    "KINGDOM/AJP_Burberry-SS_04.jpg",
-    "KINGDOM/AJP_Burberry-SS_05.jpg"
-    "KINGDOM/AJP_Burberry-SS_06.jpg"
-    "KINGDOM/AJP_Burberry-SS_08.jpg"
-    // Add more images if needed
-];
+window.onload = function() {
+    let images = [
+        "KINGDOM/AJP_Burberry-SS_03.jpg",
+        "KINGDOM/AJP_Burberry-SS_04.jpg",
+        "KINGDOM/AJP_Burberry-SS_05.jpg"
+        "KINGDOM/AJP_Burberry-SS_06.jpg"
+        "KINGDOM/AJP_Burberry-SS_08.jpg"
+        // Add more images if needed
+    ];
 
-let currentIndex = 0;
-const slideshowElement = document.getElementById('slideshow');
+    let currentIndex = 0;
+    const slideshowElement = document.getElementById('slideshow');
 
-function changeImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    slideshowElement.src = images[currentIndex];
-}
+    function changeImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        slideshowElement.src = images[currentIndex];
+    }
 
-setInterval(changeImage, 10000); // Change image every 10 seconds
+    setInterval(changeImage, 10000); // Change image every 10 seconds
+};
